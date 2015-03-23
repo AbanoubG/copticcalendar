19e8c1c3862b18cb8227b80b2622881b5f2619fc
penanko <penanko@gmail.com>
2015-03-22 21:20:42 -0700

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,27 @@
+<?php
+// This file is part of the Japanese calendar type plugin for Moodle
+//
+// The Japanese calendar type plugin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The Japanese calendar type plugin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// For a copy of the GNU General Public License, see <http://www.gnu.org/licenses/>.
+/**
+* Link to japanese calendar type settings.
+*
+* @package calendartype_copitc
+* @copyright 2008 onwards Foodle Group {@link http://foodle.org}
+* @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+*/
+defined('MOODLE_INTERNAL') || die;
+$settings->add(new admin_setting_configselect('calendartype_coptic/algorithm',
+        new lang_string('algorithm', 'calendartype_coptic'),
+        new lang_string('configalgorithm', 'calendartype_coptic'), 0,
+        array(new lang_string('algorithm1', 'calendartype_coptic'),
+              new lang_string('algorithm2', 'calendartype_coptic'))));
