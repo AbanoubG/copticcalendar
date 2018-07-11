1e01d31cc3338b5ce21fb056abb41d66b751f19b
Abanoub <penanko@gmail.com>
2018-07-11 10:56:07 -0700

    Create calendartype_coptic.php

---
+++ lang/en/calendartype_coptic.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * Strings for component 'calendartype_coptic', language 'en'.
+ *
+ * @package calendartype_coptic
+ * @copyright 2008 onwards Foodle Group {@link http://foodle.org}
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['am'] = 'am';
+$string['am_caps'] = 'AM';
+$string['name'] = 'Coptic';
+$string['month1'] = 'Thoout';
+$string['month2'] = 'Paope';
+$string['month3'] = 'Hathor';
+$string['month4'] = 'Koiahk';
+$string['month5'] = 'Tobe';
+$string['month6'] = 'Meshir';
+$string['month7'] = 'Paremhotep';
+$string['month8'] = 'Parmoute';
+$string['month9'] = 'Pashons';
+$string['month10'] = 'Paone';
+$string['month11'] = 'Epep';
+$string['month12'] = 'Mesore';
+$string['month13'] = 'Nesi';
+$string['pm'] = 'pm';
+$string['pm_caps'] = 'PM';
+$string['wday0'] = 'Sun';
+$string['wday1'] = 'Mon';
+$string['wday2'] = 'Tue';
+$string['wday3'] = 'Wed';
+$string['wday4'] = 'Thu';
+$string['wday5'] = 'Fri';
+$string['wday6'] = 'Sat';
+$string['weekday0'] = 'Sunday';
+$string['weekday1'] = 'Monday';
+$string['weekday2'] = 'Tuesday';
+$string['weekday3'] = 'Wednesday';
+$string['weekday4'] = 'Thursday';
+$string['weekday5'] = 'Friday';
+$string['weekday6'] = 'Saturday';
+$string['pluginname'] = 'Persian calendar type';
